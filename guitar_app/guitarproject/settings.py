1ded45834ddda3a0f48e94a5247ad4818933c75e
Eli <mooreep@dukes.jmu.edu>
2025-05-08 18:10:03 -0400

    1.1.5
    PostgreSQL attempt 2

--- guitar_app/guitarproject/settings.py
+++ guitar_app/guitarproject/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,10 +52,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'guitarproject.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 AUTHENTICATION_BACKENDS = [
